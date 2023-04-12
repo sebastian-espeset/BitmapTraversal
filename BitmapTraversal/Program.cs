bfcb48be7a557c3af329c7c2185ce479aee31b6a
sebas <sebastian.c.espeset@gmail.com>
2023-04-12 14:17:58 -0500

    Add project files.

---
+++ BitmapTraversal/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+Bitmap image1;
+/*
+    white:Color [A=255, R=255, G=255, B=255]
+    red:Color [A=255, R=224, G=32, B=64]
+    black:Color [A=255, R=0, G=0, B=0] 
+*/
+
+try
+{
+    image1 = new Bitmap(@"C:\Users\sebas\OneDrive\Desktop\Image01 (1).bmp");
+    int x, y;
+    List<string> centerCoordinates = new List<string>();
+    Boolean CheckAllNeighbors(Bitmap matrix, int x, int y, int value)
+    {
+        List<bool> neighbors = new List<bool>();
+        if (matrix.GetPixel(x, y + 1).B == value && matrix.GetPixel(x, y - 1).B == value)
+        {
+            neighbors.Add(true);
+        }
+        if (matrix.GetPixel(x + 1, y).B == value && matrix.GetPixel(x - 1, y).B == value)
+        {
+            neighbors.Add(true);
+        }
+        if (matrix.GetPixel(x + 1, y + 1).B == value && matrix.GetPixel(x + 1, y - 1).B == value)
+        {
+            neighbors.Add(true);
+        }
+        if (matrix.GetPixel(x - 1, y - 1).B == value && matrix.GetPixel(x - 1, y + 1).B == value)
+        {
+            neighbors.Add(true);
+        }
+
+        if (neighbors.Count == 4)
+        {
+            return true;
+        }
+        return false;
+    }
+    for (x = 0; x < image1.Width; x++)
+    {
+        for (y = 0; y < image1.Height; y++)
+        {
+            if (image1.GetPixel(x, y).B == 64)
+            {
+                if (CheckAllNeighbors(image1, x, y, 64))
+                {
+                    centerCoordinates.Add($"center coordinate found at {x},{y}");
+                }
+            }
+
+        }
+    }
+    int i;
+    for (i = 0;i<centerCoordinates.Count;i++)
+    {
+        Console.WriteLine($"{centerCoordinates[i]}");
+    }
+    
+}
+catch
+{
+    throw new Exception("There was an error");
+}
